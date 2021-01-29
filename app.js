f8a30d6497df1da67729065913615e9cecdd10c5
Kayphrosh <olakunbiolabode01@gmail.com>
2021-01-29 05:26:50 +0100

    Work in progress

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const hamburger = document.querySelector(".hamburger"); 
 const navLinks =  document.querySelector(".navbar");
 const links = document.querySelectorAll(".navbar li");
-
+var x = window.matchMedia("(max-width: 700px)");
 
 hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
+    if(navLinks.classList.toggle("open")){
     links.forEach(link =>  {
         link.classList.toggle("fade");
         hamburger.classList.toggle('toggle');
     });
-
-    
+    }else{
+    links.forEach(link =>  {
+        link.classList.toggle("fade");
+        hamburger.classList.toggle('toggle');
+        
+    });
+    }
+    if(x.matches){
+        navLinks.style.background = '#000'
+    }
+    else {
+        navLinks.style.background = '#fff'
+    }
 });
